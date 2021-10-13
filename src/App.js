1399997be5ba652d8a842394c2740dc8ee52bab4
Rodrigo Carreno <rodvader@gmail.com>
2021-10-13 17:58:39 -0300

    paque vea el pancho castillo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Overkode
+          Learn React with Overkode 2021
         </a>
       </header>
     </div>
